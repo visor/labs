cacbd2094edad550b08105152bd731897904d96d
Victor Karamzin <admin@visor.ws>
2022-10-24 05:55:33 +0700

    Лабоработрные №2. Кодирование методом Шеннона-Фано.

--- lab-encode-002/library/Helper.php
+++ lab-encode-002/library/Helper.php
@@ -6,7 +6,6 @@ class Helper
 {
     public static function getUtfLetters(string $string): array
     {
-        return str_split($string);
         $result = preg_split('//u', $string, -1, PREG_SPLIT_NO_EMPTY);
         if (false === $result) {
             return str_split($string);
